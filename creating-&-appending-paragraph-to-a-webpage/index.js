814433db4253adc982e71b6bd1e8c5549f892a6f
Krishan Kant <42918717+logicals7@users.noreply.github.com>
2021-02-01 21:14:13 +0530

    creating & appending paragraph to a webpage
    A function that creates and appends a paragraph to the page. The text for the paragraph should be: “New Paragraph Added”. Set the font color to be green and the font size to 12.

---
+++ creating-&-appending-paragraph-to-a-webpage/index.js
@@ -0,0 +1,4 @@
+function addParagraphs() {
+    var p2 = "<p style='color:green' 'font-size:12px'>New Paragraph Added</p>";
+    document.getElementById("p3").insertAdjacentHTML('beforebegin', p2);
+}
